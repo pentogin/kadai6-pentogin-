bfaf1d98f389b812bc3a1524a89c4c33662c3173
akio0911 <akio0911@gmail.com>
2021-06-22 06:52:38 +0900

    feedback

--- kadai7(pentogin)/ViewController.swift
+++ kadai7(pentogin)/ViewController.swift
@@ -9,35 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet  private weak var slider: UISlider!
-    @IBOutlet  private weak var numberLabel: UILabel!
+    @IBOutlet private weak var slider: UISlider!
+    @IBOutlet private weak var numberLabel: UILabel!
     private var correctAnswer = 0
     private static let valueRange = 1...100
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        slider.minimumValue = Float(ViewController.valueRange.lowerBound)
-        slider.maximumValue = Float(ViewController.valueRange.upperBound)
+        slider.minimumValue = Float(Self.valueRange.lowerBound)
+        slider.maximumValue = Float(Self.valueRange.upperBound)
         resetGame()
     }
     
     @IBAction func didTapjudgeButton(_ sender: Any) {
         if correctAnswer == Int(slider.value) {
-            showResultAler(msg:"あたり")
+            showResultAler(message: "あたり")
         } else {
-            showResultAler(msg:"はずれ")
+            showResultAler(message: "はずれ")
         }
-        }
-    private func showResultAler(msg: String) {
-        let alert = UIAlertController(title: "結果", message: "\(msg)\nあなたの値:\(Int(slider.value))", preferredStyle: .alert)
-        let defalutAction = UIAlertAction(title: "再挑戦", style: .default)  {_ in [weak, self];_in; ViewController?.resetGame()
     }
+
+    private func showResultAler(message: String) {
+        let alert = UIAlertController(title: "結果", message: "\(message)\nあなたの値:\(Int(slider.value))", preferredStyle: .alert)
+        let defalutAction = UIAlertAction(title: "再挑戦", style: .default) { [weak self] _ in
+            self?.resetGame()
+        }
         alert.addAction(defalutAction)
         present(alert, animated: true, completion: nil)
     }
+
     private func resetGame() {
-        correctAnswer = Int.random(in: ViewController.valueRange)
+        correctAnswer = Int.random(in: Self.valueRange)
         numberLabel.text = "\(correctAnswer)"
     }
 }
